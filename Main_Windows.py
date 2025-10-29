84245a274fc59e43501a4e6f0f8b6e1ba966f8d6
OkarRojas <orojasb@unal.edu.co>
2025-10-29 11:53:44 -0500

    Initial commit: DNM language analyzer with line number error reporting

--- Main_Windows.py
+++ Main_Windows.py
@@ -1,1 +1,13 @@
 #segunda entrega pops
+import pygame
+from pygame.locals import *
+import sys
+
+# Inicializar Pygame
+
+while True:
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    pygame.display.set_caption("Segunda Entrega POPS")
+    clock = pygame.time.Clock()
+    font = pygame.font.SysFont(None, 36)
